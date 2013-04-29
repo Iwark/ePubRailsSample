e4cc378ce0a8cd19381382a172fd37359caff4b9
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-04-29 19:05:34 +0900

    authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epubapi::Application.config.secret_key_base = '502c5c21b05384a4aa645179a14b03c1b97bb2a8f1d779506bde1929751ef39b98c1d83ea71d5a4f21235a0fae1c2c6036ae95db7b5fb1eba327eb036437d3a9'
