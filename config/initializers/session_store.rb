34d78aa3102e5265a514bd91003c1d5066d709fb
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-04-29 19:30:12 +0900

    aa

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Epubapi::Application.config.session_store :encrypted_cookie_store, key: '_epubapi_session'
+Epubapi::Application.config.session_store :encrypted_store, key: '_epubapi_session'
